a479f90c9e43fa7be71af846aafe9aab8feae943
Tim Shaffer <timshaffer@me.com>
2014-03-25 22:51:38 -0400

    Don't need to check debug here.

--- gallery/urls.py
+++ gallery/urls.py
@@ -11,6 +11,5 @@ urlpatterns = patterns(
     url(r'^', include('photos.urls')),
 )
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
