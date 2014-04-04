3cd93d91fcd61aa0892c7aac9a334d558604f97a
Tim Shaffer <timshaffer@me.com>
2014-04-04 11:40:10 -0400

    Import views instead of using strings.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls import patterns, url
 
+from accounts import views
+
 urlpatterns = patterns(
-    'accounts.views',
-    url(r'^login/$', 'login', name='login'),
-    url(r'^logout/$', 'logout', name='logout'),
-    url(r'^register/$', 'register', name='register'),
-    url(r'^register/(?P<auth>[\w]+)/$', 'register', name='register'),
-    url(r'^profile/$', 'profile', name='profile'),
-    url(r'^password/reset/$', 'password_reset', name='password_reset'),
+    '',
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^register/(?P<auth>[\w]+)/$', views.register, name='register'),
+    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^password/reset/$', views.password_reset, name='password_reset'),
     url(r'^password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        'password_reset_confirm', name='password_reset_confirm'),
-    url(r'^password/change/$', 'password_change', name='password_change'),
+        views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^password/change/$', views.password_change, name='password_change'),
 )
